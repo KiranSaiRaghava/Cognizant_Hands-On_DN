f15dc77e1343eee16c8361354139cfc00cda4c30
KIRAN SAI RAGHAVA KORAGANJI <23501a4421@pvpsit.ac.in>
2026-07-03 06:35:48 +0530

    Add files via upload

---
+++ week-2/Spring Core Uisng Maven/Excercise_2/src/main/java/com/library/MainApp.java
@@ -0,0 +1,18 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import com.library.service.BookService;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load the Spring XML configuration file
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        
+        // Retrieve the BookService bean from the container
+        BookService bookService = (BookService) context.getBean("bookService");
+        
+        // Call the method to verify dependency injection worked
+        bookService.displayService();
+    }
+}
