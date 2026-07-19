62ef80ead768aefd3795859441ef2bfef5ce4a52
Kiran Sai Raghava <23501a4421@pvpsit.ac>
2026-07-19 06:12:49 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to the first session of React</h1>
+    </div>
+  );
+}
+
+export default App;
